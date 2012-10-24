3ea1aaea283ef176d9d89dfa7d5c54c6b008f97e
Taihao Zhang <jason@zth.me>
2012-10-24 23:13:01 +1100

    Run cache compiler pass.

--- src/ANU/Bundle/StyleProxyBundle/ANUStyleProxyBundle.php
+++ src/ANU/Bundle/StyleProxyBundle/ANUStyleProxyBundle.php
@@ -3,7 +3,14 @@
 namespace ANU\Bundle\StyleProxyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ANUStyleProxyBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new DependencyInjection\Compiler\CachePass());
+    }
 }
