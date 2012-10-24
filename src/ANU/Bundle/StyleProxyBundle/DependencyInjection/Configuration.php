8f781ad7abffbf18151f69db0b4e549954f8e2a1
Taihao Zhang <jason@zth.me>
2012-10-24 20:47:26 +1100

    Process proxy mode config option.

--- src/ANU/Bundle/StyleProxyBundle/DependencyInjection/Configuration.php
+++ src/ANU/Bundle/StyleProxyBundle/DependencyInjection/Configuration.php
@@ -20,9 +20,16 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('anu_style_proxy');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->scalarNode('proxy_mode')
+                    ->isRequired()
+                    ->validate()
+                        ->ifNotInArray(array('style', 'asset', 'combined'))
+                        ->thenInvalid('Invalid proxy mode "%s"')
+                    ->end()
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
