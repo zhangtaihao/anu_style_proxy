f64faae261884f0b0787ec0452beb8f0e81c9853
Taihao Zhang <jason@zth.me>
2012-10-25 12:52:10 +1100

    Remove utility bundles.

--- src/ZthMe/AssetMirrorBundle/ZthMeAssetMirrorBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ZthMe\AssetMirrorBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ZthMeAssetMirrorBundle extends Bundle
-{
-}
--- src/ZthMe/ResourceHandlerBundle/ZthMeResourceHandlerBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ZthMe\ResourceHandlerBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ZthMeResourceHandlerBundle extends Bundle
-{
-}
