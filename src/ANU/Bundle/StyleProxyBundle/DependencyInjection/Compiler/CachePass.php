7d812ed44ee89f33e1082367d755506919dacb46
Taihao Zhang <jason@zth.me>
2012-10-27 23:49:29 +1100

    Alias cache service.

--- src/ANU/Bundle/StyleProxyBundle/DependencyInjection/Compiler/CachePass.php
+++ src/ANU/Bundle/StyleProxyBundle/DependencyInjection/Compiler/CachePass.php
@@ -3,7 +3,6 @@
 namespace ANU\Bundle\StyleProxyBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CachePass implements CompilerPassInterface
@@ -18,7 +17,6 @@ class CachePass implements CompilerPassInterface
             throw new \InvalidArgumentException('Supplied cache ID is not defined: '.$cacheId);
         }
 
-        $cacheDefinition = $container->getDefinition('anu_style_proxy.cache');
-        $cacheDefinition->addArgument(new Reference($cacheId));
+        $container->setAlias('anu_style_proxy.cache_id', $cacheId);
     }
 }
