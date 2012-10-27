3a941da36d425028ceb7aa0493cedd6726e7c4f6
Taihao Zhang <jason@zth.me>
2012-10-27 19:30:28 +1100

    Implement backend style server delegation.

--- src/ANU/Bundle/StyleProxyBundle/Proxy/StyleServer.php
+++ src/ANU/Bundle/StyleProxyBundle/Proxy/StyleServer.php
@@ -3,21 +3,38 @@
 namespace ANU\Bundle\StyleProxyBundle\Proxy;
 
 use Guzzle\Http\Client;
+use Guzzle\Http\Message\RequestInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class StyleServer
 {
-    protected $backendStyleBase;
+    private $backendStyleBase;
+    private $backendStyleInclude;
 
+    /** @var Client */
     private $client;
 
     public function setBackendStyleBase($baseUrl)
     {
         $this->backendStyleBase = $baseUrl;
+        if (isset($this->client)) {
+            $this->client->setBaseUrl($baseUrl);
+        }
     }
 
     public function getBackendStyleBase()
     {
         return $this->backendStyleBase;
+    }
+
+    public function setBackendStyleInclude($backendStyleInclude)
+    {
+        $this->backendStyleInclude = $backendStyleInclude;
+    }
+
+    public function getBackendStyleInclude()
+    {
+        return $this->backendStyleInclude;
     }
 
     public function getBackendClient()
@@ -26,5 +43,18 @@ class StyleServer
             $this->client = new Client($this->backendStyleBase);
         }
         return $this->client;
+    }
+
+    public function delegate(Request $request)
+    {
+        $backendRequest = $this->createBackendRequest($request);
+        return $backendRequest->send();
+    }
+
+    protected function createBackendRequest(Request $request)
+    {
+        $uri = $this->backendStyleInclude.'?'.$request->getQueryString();
+        $backendRequest = $this->getBackendClient()->createRequest(RequestInterface::GET, $uri);
+        return $backendRequest;
     }
 }
