77ac237016b0a5f4fd395a7b58d22e68df63232c
Taihao Zhang <jason@zth.me>
2012-10-27 19:00:10 +1100

    Validate conditional configuration.

--- src/ANU/Bundle/StyleProxyBundle/DependencyInjection/ANUStyleProxyExtension.php
+++ src/ANU/Bundle/StyleProxyBundle/DependencyInjection/ANUStyleProxyExtension.php
@@ -3,6 +3,7 @@
 namespace ANU\Bundle\StyleProxyBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -28,16 +29,27 @@ class ANUStyleProxyExtension extends Extension
         $container->setParameter('anu_style_proxy.backend_style_server', $config['backend_style_server']);
         $container->setParameter('anu_style_proxy.backend_asset_server', $config['backend_asset_server']);
 
-        $this->registerStyleServerConfiguration($config, $container, $loader);
+        if (!isset($config['backend_style_server'])) {
+            throw new InvalidArgumentException('Backend style server is required.');
+        }
+        if (!isset($config['backend_asset_server']) && $config['proxy_mode'] == 'style') {
+            throw new InvalidArgumentException('Backend asset server is required.');
+        }
+
+        if (in_array($config['proxy_mode'], array('style', 'combined'))) {
+            $this->registerStyleServerConfiguration($config, $container, $loader);
+        }
+
         $this->registerAssetServerConfiguration($config, $container, $loader);
-        $this->registerAssetManagerConfiguration($config, $container, $loader);
+
+        if (in_array($config['proxy_mode'], array('asset', 'combined'))) {
+            $this->registerAssetManagerConfiguration($config, $container, $loader);
+        }
     }
 
     private function registerStyleServerConfiguration($config, ContainerBuilder $container, YamlFileLoader $loader)
     {
-        if (in_array($config['proxy_mode'], array('style', 'combined'))) {
-            $loader->load('style_server.yml');
-        }
+        $loader->load('style_server.yml');
     }
 
     private function registerAssetServerConfiguration($config, ContainerBuilder $container, YamlFileLoader $loader)
@@ -47,8 +59,6 @@ class ANUStyleProxyExtension extends Extension
 
     private function registerAssetManagerConfiguration($config, ContainerBuilder $container, YamlFileLoader $loader)
     {
-        if (in_array($config['proxy_mode'], array('style', 'combined'))) {
-            $loader->load('asset_manager.yml');
-        }
+        $loader->load('asset_manager.yml');
     }
 }
