7ef48a1be58397e91bb676ca0cf6317e5cadc614
Taihao Zhang <jason@zth.me>
2012-10-27 20:43:30 +1100

    Return framework response for delegate request.

--- src/ANU/Bundle/StyleProxyBundle/Proxy/StyleServer.php
+++ src/ANU/Bundle/StyleProxyBundle/Proxy/StyleServer.php
@@ -3,8 +3,10 @@
 namespace ANU\Bundle\StyleProxyBundle\Proxy;
 
 use Guzzle\Http\Client;
-use Guzzle\Http\Message\RequestInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Guzzle\Http\Message\RequestInterface as GuzzleRequestInterface;
+use Guzzle\Http\Message\Response as GuzzleResponse;
 
 /**
  * Delegate style server.
@@ -55,19 +57,37 @@ class StyleServer
     /**
      * Delegates a request to the backend server.
      *
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     * @return \Guzzle\Http\Message\Response
+     * @param Request $request
+     * @return Response
      */
     public function delegate(Request $request)
     {
         $backendRequest = $this->createBackendRequest($request);
-        return $backendRequest->send();
+        $backendResponse = $backendRequest->send();
+        return $this->createResponse($backendResponse);
     }
 
+    /**
+     * Creates a backend request from a Symfony framework request.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Guzzle\Http\Message\RequestInterface
+     */
     protected function createBackendRequest(Request $request)
     {
         $uri = $this->backendStyleInclude.'?'.$request->getQueryString();
-        $backendRequest = $this->getBackendClient()->createRequest(RequestInterface::GET, $uri);
+        $backendRequest = $this->getBackendClient()->createRequest(GuzzleRequestInterface::GET, $uri);
         return $backendRequest;
+    }
+
+    /**
+     * Creates a Symfony framework response from a backend response.
+     *
+     * @param \Guzzle\Http\Message\Response $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function createResponse(GuzzleResponse $response)
+    {
+        return Response::create($response->getBody(), $response->getStatusCode(), $response->getHeaders()->getAll());
     }
 }
