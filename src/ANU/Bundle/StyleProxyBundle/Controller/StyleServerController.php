bcc485e4c6fcd5873067e9a829fdc678851e4a15
Taihao Zhang <jason@zth.me>
2012-10-27 20:45:41 +1100

    Implement basic style server include response.

--- src/ANU/Bundle/StyleProxyBundle/Controller/StyleServerController.php
+++ src/ANU/Bundle/StyleProxyBundle/Controller/StyleServerController.php
@@ -4,11 +4,17 @@ namespace ANU\Bundle\StyleProxyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use ANU\Bundle\StyleProxyBundle\Proxy\StyleServer;
 
 class StyleServerController extends Controller
 {
     public function includeAction()
     {
+        $request = $this->getRequest();
 
+        /** @var $styleServer StyleServer */
+        $styleServer = $this->get('anu_style_proxy.style_server');
+
+        return $styleServer->delegate($request);
     }
 }
