01f7d50b1f51aa881fab6c48ef58f8c8566b0252
Taihao Zhang <jason@zth.me>
2012-10-27 22:15:09 +1100

    Remove AppCache loading.

--- web/app.php
+++ web/app.php
@@ -13,7 +13,7 @@ if (extension_loaded('apc')) {
 }
 
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
+//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
