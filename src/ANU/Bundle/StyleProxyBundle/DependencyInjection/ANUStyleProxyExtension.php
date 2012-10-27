73f951ff2821199dab1bda70bd762e7b010c6f1f
Taihao Zhang <jason@zth.me>
2012-10-27 18:53:14 +1100

    Split server configuration loading into separate methods.

--- src/ANU/Bundle/StyleProxyBundle/DependencyInjection/ANUStyleProxyExtension.php
+++ src/ANU/Bundle/StyleProxyBundle/DependencyInjection/ANUStyleProxyExtension.php
@@ -5,7 +5,7 @@ namespace ANU\Bundle\StyleProxyBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -22,18 +22,28 @@ class ANUStyleProxyExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
-        // Load style server.
+        $this->registerStyleServerConfiguration($config, $container, $loader);
+        $this->registerAssetServerConfiguration($config, $container, $loader);
+        $this->registerAssetManagerConfiguration($config, $container, $loader);
+    }
+
+    private function registerStyleServerConfiguration($config, ContainerBuilder $container, YamlFileLoader $loader)
+    {
         if (in_array($config['proxy_mode'], array('style', 'combined'))) {
             $loader->load('style_server.yml');
         }
+    }
 
-        // Load asset server.
+    private function registerAssetServerConfiguration($config, ContainerBuilder $container, YamlFileLoader $loader)
+    {
         $loader->load('asset_server.yml');
+    }
 
-        // Load asset manager.
+    private function registerAssetManagerConfiguration($config, ContainerBuilder $container, YamlFileLoader $loader)
+    {
         if (in_array($config['proxy_mode'], array('style', 'combined'))) {
             $loader->load('asset_manager.yml');
         }
