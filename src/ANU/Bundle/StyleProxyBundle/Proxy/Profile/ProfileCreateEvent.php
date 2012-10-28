8bf1f626e845bf059025aeeda56b67688b1f4b4a
Taihao Zhang <jason@zth.me>
2012-10-28 15:12:46 +1100

    Add event classes.

---
+++ src/ANU/Bundle/StyleProxyBundle/Proxy/Profile/ProfileCreateEvent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ANU\Bundle\StyleProxyBundle\Proxy\Profile;
+
+/**
+ * Event triggered when a profile has been created by the profile handler.
+ */
+class ProfileCreateEvent extends ProfileEvent {}
