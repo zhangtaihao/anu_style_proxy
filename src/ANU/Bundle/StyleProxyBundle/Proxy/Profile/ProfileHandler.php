41fa01bb4e3438a3c4de55e2a84710f9b12df271
Taihao Zhang <jason@zth.me>
2012-10-28 11:55:14 +1100

    Add profile handler implementation.

---
+++ src/ANU/Bundle/StyleProxyBundle/Proxy/Profile/ProfileHandler.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace ANU\Bundle\StyleProxyBundle\Proxy\Profile;
+
+use Doctrine\Common\Cache\Cache;
+use ANU\Bundle\StyleProxyBundle\Exception\ProfileNotFoundException;
+use ANU\Bundle\StyleProxyBundle\Exception\ProfileNotCachedException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Handler for creating and processing profiles and retrieving them from cache.
+ */
+class ProfileHandler
+{
+    /**
+     * Application cache service.
+     * @var Cache
+     */
+    protected $cache;
+
+    public function __construct(Cache $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    /**
+     * Gets a profile for a query.
+     *
+     * @param array $query
+     *   Query parameters for the profile.
+     *
+     * @throws ProfileNotFoundException
+     *   If the query does not map to a profile object.
+     */
+    public function getProfile(array $query)
+    {
+        return $this->lookupCache($query);
+    }
+
+    /**
+     * Creates a profile from data.
+     *
+     * @param mixed $data
+     *   JSON string or array data for profile.
+     * @param bool $cache
+     *   Whether to cache the profile for retrieval via self::getProfile().
+     * @return Profile
+     *   Created profile object.
+     *
+     * @throws \InvalidArgumentException
+     *   If the given data cannot be used to create the profile.
+     */
+    public function createProfile($data, $cache = true)
+    {
+        $profile = new Profile($data);
+
+        // TODO Process profile.
+
+        return $profile;
+    }
+
+    /**
+     * Looks up a cached entry.
+     */
+    protected function lookupCache(array $query)
+    {
+        $id = $this->getNormalizedCacheId($query);
+        if (isset($this->cache) && $this->cache->contains($id)) {
+            $json = $this->cache->fetch($id);
+            return new Profile($json);
+        }
+        throw new ProfileNotCachedException('Profile cache does not exist or has expired.');
+    }
+
+    /**
+     * Gets a normalized caching identifier for a query.
+     */
+    protected function getNormalizedCacheId(array $query)
+    {
+        // Remove part.
+        if (isset($query['part'])) {
+            unset($query['part']);
+        }
+        // Sort by key.
+        ksort($query);
+        // Compute hash to finish.
+        return 'profile:'.md5(serialize($query));
+    }
+}
