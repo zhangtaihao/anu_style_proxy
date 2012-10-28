2b0a3bfd831999a83be293e91718970634403935
Taihao Zhang <jason@zth.me>
2012-10-28 13:36:46 +1100

    Test retrieving profile for invalid request.

--- src/ANU/Bundle/StyleProxyBundle/Tests/Proxy/StyleServerTest.php
+++ src/ANU/Bundle/StyleProxyBundle/Tests/Proxy/StyleServerTest.php
@@ -46,4 +46,17 @@ class StyleServerTest extends \PHPUnit_Framework_TestCase
         // Call again to ensure invocation only once.
         $server->getProfileForRequest($request);
     }
+
+    /**
+     * Style server proxy does not return a profile for an invalid request.
+     *
+     * @expectedException \ANU\Bundle\StyleProxyBundle\Exception\InvalidProfileRequestException
+     */
+    public function testGetInvalidProfile()
+    {
+        $handler = new ProfileHandler(new ArrayCache());
+        $server = new StyleServer('http://styles.anu.edu.au/_anu', 'include.php', $handler);
+        $request = new Request();
+        $server->getProfileForRequest($request);
+    }
 }
