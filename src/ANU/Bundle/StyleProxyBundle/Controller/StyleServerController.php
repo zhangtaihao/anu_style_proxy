dccad1ef1571c89f852d044c9569ecda9495fe7f
Taihao Zhang <jason@zth.me>
2012-10-30 06:20:02 +1100

    Revert to setter injection.

--- src/ANU/Bundle/StyleProxyBundle/Controller/StyleServerController.php
+++ src/ANU/Bundle/StyleProxyBundle/Controller/StyleServerController.php
@@ -10,13 +10,6 @@ class StyleServerController extends Controller
 {
     protected $cacheMaxAge;
 
-    public function __construct($cacheEnabled, $cacheMaxAge)
-    {
-        if ($cacheEnabled) {
-            $this->cacheMaxAge = $cacheMaxAge;
-        }
-    }
-
     public function setResponseCache($enabled, $maxAge)
     {
         if (!$enabled) {
