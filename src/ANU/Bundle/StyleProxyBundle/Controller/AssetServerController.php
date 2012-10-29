79a8d021d3f934e0c804b6f4ff4c079d8ebfb27b
Taihao Zhang <jason@zth.me>
2012-10-30 04:40:46 +1100

    Implement asset server controller.

--- src/ANU/Bundle/StyleProxyBundle/Controller/AssetServerController.php
+++ src/ANU/Bundle/StyleProxyBundle/Controller/AssetServerController.php
@@ -2,12 +2,33 @@
 
 namespace ANU\Bundle\StyleProxyBundle\Controller;
 
+use ANU\Bundle\StyleProxyBundle\Proxy\AssetServer;
+use ANU\Bundle\StyleProxyBundle\Proxy\BaseUrl;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AssetServerController extends Controller
 {
-    public function assetAction()
-    {
+    protected $backendBaseUrl;
 
+    public function __construct(BaseUrl $backendBaseUrl)
+    {
+        $this->backendBaseUrl = $backendBaseUrl;
+    }
+
+    public function resourceAction($path)
+    {
+        $request = $this->getRequest();
+
+        // Delegate to asset server to retrieve backend.
+        if ($this->has('anu_style_proxy.asset_server')) {
+            /** @var $server AssetServer */
+            $server = $this->get('anu_style_proxy.asset_server');
+            return $server->getResource($request);
+        }
+        // Redirect to backend.
+        else {
+            $url = rtrim($this->backendBaseUrl->getUrl(), '/').'/'.$path;
+            return $this->redirect($url);
+        }
     }
 }
