3c63a8b2fd7cac34d90b62bd3f22323613c44339
Taihao Zhang <jason@zth.me>
2012-11-01 11:49:46 +1100

    Cache placeholder page.

--- src/ANU/Bundle/StyleProxyBundle/Controller/DefaultController.php
+++ src/ANU/Bundle/StyleProxyBundle/Controller/DefaultController.php
@@ -3,9 +3,13 @@
 namespace ANU\Bundle\StyleProxyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Cache(maxage="2592000")
+     */
     public function indexAction()
     {
         return $this->render('ANUStyleProxyBundle:Default:index.html.twig', array(
